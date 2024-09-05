2917c17b54175f5fe7712d1d620f5c5ea3d53de2
PierreTDX <pierre.tondeux@gmail.com>
2024-09-05 18:27:21 +0200

    selection/suppression des items dans les menus + ajout ou suppression des tag OK

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,10 +1,9 @@
 import { openMenu } from "../utils/openMenu.js";
-import { selectItem, deleteSelectedItem } from "../utils/selectItemMenu.js";
+import { selectItem} from "../utils/selectItemMenu.js";
 import { createTag } from "../utils/createTag.js";
 import { deleteTag } from "../utils/deleteTag.js";
 
 openMenu();
 selectItem();
-deleteSelectedItem();
 createTag();
 deleteTag();
