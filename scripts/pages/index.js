9d2d1c19b889cb2cb834dbc3a0ded79c274a3830
PierreTDX <pierre.tondeux@gmail.com>
2024-09-06 16:07:02 +0200

    connexion à l'API et fichier des datas converti en Json. Les datas sont stockées dans "recipesData"

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,11 +1,22 @@
+import Api from "../api/Api.js";
 import { openMenu } from "../utils/openMenu.js";
 import { selectItem} from "../utils/selectItemMenu.js";
 import { createTag } from "../utils/createTag.js";
 import { deleteTag } from "../utils/deleteTag.js";
 import { counterRecipes } from "../utils/counterRecipes.js"; 
 
+const recipesApi = new Api("./data/recipes.json");
+
+const init = async () => {
+    const recipesData = await recipesApi.get();
+    console.log("dans index.js",recipesData);
+    
+};
+
+init();
+
 openMenu();
 selectItem();
 createTag();
 deleteTag();
-counterRecipes();
+counterRecipes()
